904b30ab6e80e1fc99f4f5a7723d6cb54654d100
Felipe Cortez <felipecortezfi@gmail.com>
2015-02-27 20:16:25 +0000

    Removed class structure, added fancy tree printing

--- tp.py
+++ tp.py
@@ -5,6 +5,15 @@ import sys
 def is_term(t):
     return re.match(r"[A-Z]+[0-9]*", t) is not None
 
+def print_tree(node, level = 0):
+    if node is not None:
+        if type(node) is Node:
+            print("." * 4 * level + node.root)
+            for n in node.children:
+                print_tree(n, level + 1)
+        else:
+            print("." * 4 * level + node)
+
 class Node():
     def __init__(self, root, children):
         self.root = root
@@ -13,82 +22,79 @@ class Node():
     def __repr__(self):
         return "{R: " + self.root + ", C: " + str(self.children) + "}"
 
-class Parser():
-    def __init__(self, formula):
-        # Taken from Norvig's "(How to Write a (Lisp) Interpreter (in Python))"
-        self.tokens = formula.replace('(', ' ( ') \
-                             .replace(')', ' ) ') \
-                             .replace('-', ' - ') \
-                             .split()
-        self.op_stack = []
-        self.out_stack = []
-        self.tree = None
+def parse(astr):
+    # Taken from Norvig's "(How to Write a (Lisp) Interpreter (in Python))"
+    tokens = astr.replace('(', ' ( ') \
+                 .replace(')', ' ) ') \
+                 .replace('-', ' - ') \
+                 .split()
+    op_stack = []
+    out_stack = []
 
-    def formula(self):
-        for token in self.tokens:
-            print(token)
-            print(self.out_stack)
-            print(self.op_stack)
-            print('---')
+    for token in tokens:
+        print(token)
+        print(out_stack)
+        print(op_stack)
+        print('---')
 
-            if is_term(token):
-                self.out_stack.append(token)
-            elif token == '-':
-                self.op_stack.append(token)
-            elif token in ['<=>',
-                            '=>',
-                            '|',
-                            '&']:
-                self.op_stack.append(token)
-            elif token == '(':
-                self.op_stack.append(token)
-            elif token == ')':
-                while self.op_stack and self.op_stack[-1] != '(':
-                    if self.op_stack[-1] == '-':
-                        try:
-                            self.out_stack.append(Node(self.op_stack.pop(),
-                                                self.out_stack.pop()))
-                        except:
-                            print("Invalid format")
-                            return
+        if is_term(token):
+            out_stack.append(token)
+        elif token == '-':
+            op_stack.append(token)
+        elif token in ['<=>',
+                        '=>',
+                        '|',
+                        '&']:
+            op_stack.append(token)
+        elif token == '(':
+            op_stack.append(token)
+        elif token == ')':
+            while op_stack and op_stack[-1] != '(':
+                if op_stack[-1] == '-':
+                    try:
+                        out_stack.append(Node(op_stack.pop(),
+                                            out_stack.pop()))
+                    except:
+                        print("Invalid format")
+                        return
 
-                    else:
-                        try:
-                            self.out_stack.append(Node(self.op_stack.pop(),
-                                                [self.out_stack.pop(),
-                                                self.out_stack.pop()][::-1]))
-                        except:
-                            print("Invalid format")
-                            return
-
-                if self.op_stack:
-                    self.op_stack.pop()
                 else:
-                    print("Mismatched parenthesis")
-                    return
-            else:
-                print("Invalid token:", token)
-                return
+                    try:
+                        out_stack.append(Node(op_stack.pop(),
+                                            [out_stack.pop(),
+                                            out_stack.pop()][::-1]))
+                    except:
+                        print("Invalid format")
+                        return
 
-        while self.op_stack:
-            if self.op_stack[-1] != '(':
-                self.out_stack.append(self.op_stack.pop())
+            if op_stack:
+                op_stack.pop()
             else:
                 print("Mismatched parenthesis")
                 return
-
-        if len(self.out_stack) == 1:
-            return self.out_stack[0]
         else:
-            print("Invalid format")
-            return None
+            print("Invalid token:", token)
+            return
+
+    while op_stack:
+        if op_stack[-1] != '(':
+            out_stack.append(op_stack.pop())
+        else:
+            print("Mismatched parenthesis")
+            return
+
+    if len(out_stack) == 1:
+        return out_stack[0]
+    else:
+        print("Invalid format")
+        return
 
 def main(argv=None):
     if argv:
         astr = argv[0]
 
-    ast = Parser(astr).formula()
-    print(ast)
+    ast = parse(astr)
+    print_tree(ast)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
