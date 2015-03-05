0bc4fc1a20c81bad29a0dc210fc28781c90378e9
Felipe Cortez <felipecortezfi@gmail.com>
2015-03-05 01:45:19 +0000

    Added negation and De Morgan and fixed things
    At least I think I fixed them

--- tp.py
+++ tp.py
@@ -1,24 +1,30 @@
 import re
 import argparse
 import sys
-from copy import deepcopy
+
+def is_op(t):
+    return t in ["<=>", "=>", "|", "&"]
 
 def is_term(t):
     return re.match(r"[A-Z]+[0-9]*", t) is not None
 
 def print_tree(node, level = 0):
     if node is not None:
-        if type(node) is Node:
-            print("." * 4 * level + node.root)
-            for n in node.children:
-                print_tree(n, level + 1)
-        else:
-            print("." * 4 * level + node)
+        print("." * 4 * level + node.root)
+        for n in node.children:
+            print_tree(n, level + 1)
 
 class Node(object):
-    def __init__(self, root, children):
-        self.root = root
-        self.children = children
+    def __init__(self, root, children=[]):
+        if type(root) == str:
+            self.root = root
+            self.children = children
+        elif type(root) == Node:
+            self.root = root.root
+            self.children = root.children
+        elif type(root) == list:
+            self.root = root.root
+            self.children = root.children
 
     def __repr__(self):
         return "{R: " + self.root + ", C: " + str(self.children) + "}"
@@ -42,14 +48,11 @@ def parse(astr):
             out_stack.append(token)
         elif token == "-":
             op_stack.append(token)
-        elif token in ["<=>",
-                       "=>",
-                       "|",
-                       "&"]:
+        elif is_op(token):
             while op_stack and op_stack[-1] == "-":
                 try:
                     out_stack.append(Node(op_stack.pop(),
-                                            [out_stack.pop()]))
+                                            [Node(out_stack.pop())]))
                 except:
                     print("Invalid format")
                     return
@@ -63,16 +66,18 @@ def parse(astr):
                 if op_stack[-1] == "-":
                     try:
                         out_stack.append(Node(op_stack.pop(),
-                                              [out_stack.pop()]))
+                                              [Node(out_stack.pop())]))
                     except:
                         print("Invalid format")
                         return
 
                 else:
                     try:
+                        print(op_stack)
+                        print(out_stack)
                         out_stack.append(Node(op_stack.pop(),
-                                             [out_stack.pop(),
-                                             out_stack.pop()][::-1]))
+                                             [Node(out_stack.pop()),
+                                              Node(out_stack.pop())][::-1]))
                     except:
                         print("Invalid format")
                         return
@@ -100,22 +105,40 @@ def parse(astr):
         return
 
 def trav1(node):
-    if type(node) == Node:
-        if node.root == "=>":
-            node.root = "|"
-            node.children[0] = Node("-", [node.children[0]])
-        elif node.root == "<=>":
-            node.root = "&"
-            #node.children = [Node("=>", [node.children[0], node.children[1]]),
-                             #Node("=>", [node.children[1], node.children[0]])]
-            node.children = [Node("|", [Node("-", [node.children[0]]), node.children[1]]),
-                             Node("|", [Node("-", [node.children[1]]), node.children[0]])]
+    if node.root == "=>":
+        node.root = "|"
+        node.children[0] = Node("-", [Node(node.children[0])])
+    elif node.root == "<=>":
+        node.root = "&"
+        node.children = [Node("|", [Node("-", [node.children[0]]), Node(node.children[1])]),
+                            Node("|", [Node("-", [node.children[1]]), Node(node.children[0])])]
 
-        for n in node.children:
-            trav1(n)
+    for n in node.children:
+        trav1(n)
+
+def trav2(node):
+    if node.root == "-":
+        ch = node.children[0]
+        if not is_term(ch.root):
+            if ch.root == "-":
+                new_node = Node(ch.children[0])
+                node.root = new_node.root
+                node.children = new_node.children
+            elif ch.root == "|":
+                new_node = Node("&", [Node("-", [ch.children[0]]), Node("-", [ch.children[1]])])
+                node.root = new_node.root
+                node.children = new_node.children
+            elif ch.root == "&":
+                new_node = Node("|", [Node("-", [ch.children[0]]), Node("-", [ch.children[1]])])
+                node.root = new_node.root
+                node.children = new_node.children
+
+    for n in node.children:
+        trav2(n)
 
 def cnfize(ast):
     trav1(ast)
+    trav2(ast)
 
 def main(argv=None):
     if argv:
